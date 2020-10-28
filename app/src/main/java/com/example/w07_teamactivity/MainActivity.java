980a314234a913957d551935477ffe54b100ead2
Allan Marina <allanmarina33@gmail.com>
2020-10-28 13:55:24 -0700

    team conflict of W07 part 2

--- app/src/main/java/com/example/w07_teamactivity/MainActivity.java
+++ app/src/main/java/com/example/w07_teamactivity/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     public void nothing(){
         System.out.println("This is a test");
         System.out.println("New test!");
-        System.out.println("first team conflict. ");
+        System.out.println("first team conflict created by.... ");
         System.out.println("Conflict by Rufino");
     }
 }
